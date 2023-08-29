30b7230ae3631081b069e9d152ae3da0fa728885
namenaro <nanenaro@gmail.com>
2023-08-29 18:14:05 +0300

    rdtgjrfu

--- EXP1_R.py
+++ EXP1_R.py
@@ -8,27 +8,28 @@ def get_data():
     return start_situation.signal, start_situation.start_point
 
 
-def make_exp_for_points(fixed_points, real_points_list, signal, log):
+def make_exp_for_scene(fixed_points, filled_scene, log):
     fig, ax = plt.subplots()
     draw_ECG(ax, signal)
     draw_vertical_line(ax, x=start_point, y=max(signal), label="start_point")
     log.add_fig(fig)
 
-    r_eval = REval(signal, fixed_coords=fixed_points, coords_real=real_points_list)
+    r_eval = REval(fixed_coords=fixed_points, filled_scene_to_eval=filled_scene)
     R = r_eval.get_R()
     log.add_text("R = " + R)
     print ("R= " + R)
+
 
 
 if __name__ == '__main__':
     signal, start_point = get_data()
 
     log = HtmlLogger("R_LOG")
+    fixed_points = [start_point]
 
-    fixed_coords = [start_point]
     best_real_points = [15, 5, 37, 39]
-    make_exp_for_points(fixed_coords, real_points_list=best_real_points, signal=signal, log=log)
+    make_exp_for_scene(fixed_points, filled_scene, log)
 
     bad_real_points = [60, 70, 80, 90]
-    make_exp_for_points(fixed_coords, real_points_list=bad_real_points, signal=signal, log=log)
+    make_exp_for_scene(fixed_points, filled_scene, log)
 
--- eval_r.py
+++ eval_r.py
@@ -1,20 +1,41 @@
-
+from scene import Scene
+from utils import Distr
 
 
 # Есть некий сигнал. К нему даны n фиксированных точкек.
 # Нужно составит ьраспределение интерполяционного качества для m точек, из которых
 # n уже поставлены. Все m точек в интерполяторе по сигналу соединяются по приниципу ближайших соседей справа и слева.
 
+
+# r ошибка интерполяции на данном сигнале при данных точках
+# R вероятностная нетривиальность такой маленькой ошибки при такой вводной
+
+
 class REval:
-    def __init__(self, signal, fixed_coords, coords_real):
-        self.signal = signal
+    def __init__(self, fixed_coords, filled_scene_to_eval):
+        self.signal = filled_scene_to_eval.signal
         self.fixed_coords = fixed_coords
-        self.coords_real = coords_real
+        self.num_coords_real = filled_scene_to_eval.num_points()
+        self.real_r = filled_scene_to_eval.get_err_sum()
 
         self.r_distr = self._get_r_distr()
 
+
     def _get_r_distr(self):
+        N=200
+        rs_sample = []
+        for _ in range(N):
+            random_points =
+            r = self._get_r(random_points)
+            rs_sample.append((r))
+        r_distr = Distr(rs_sample)
         return r_distr
 
     def get_R(self):
+        R = self.r_distr.p_event_more_eq_than_val(self.real_r)
         return R
+
+    def _get_r(self, points):
+        # сортируем токчки, теперь у каждой (кроме конечных есть ближайший сосед слева и справа)
+        scene = Scene()
+
